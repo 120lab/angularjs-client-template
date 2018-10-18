d82044255fb4136f96f53067785fbbf697b75e1e
crmbots <crmbots@gmail.com>
2018-10-19 00:18:57 +0300

    Create order

--- app/products/products.controller.js
+++ app/products/products.controller.js
@@ -1,19 +1,58 @@
-app.controller('productsController', function($scope, $mdDialog, $mdToast, productsFactory){
- 
+app.controller('productsController', function ($scope, $mdDialog, $mdToast, productsFactory) {
+
+    // show toast message
+    $scope.showToast = function (message) {
+        $mdToast.show(
+            $mdToast.simple()
+                .textContent(message)
+                .hideDelay(3000)
+                .position("top right")
+        );
+    }
+
     // read products
-    $scope.readProducts = function(){
- 
+    $scope.readProducts = function () {
+
         // use products factory
-        productsFactory.readProducts().then(function successCallback(response){
+        productsFactory.readProducts().then(function successCallback(response) {
             console.log(response.data);
             $scope.products = response.data;
-        }, function errorCallback(response){
+        }, function errorCallback(response) {
             $scope.showToast("Unable to read record.");
         });
- 
+
     }
-     
-    // showCreateProductForm will be here
-     
-    // DialogController will be here
+
+    $scope.showConfirm = function(Description, Id, Value , Amount) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+              .title('Order confirmation')
+              .htmlContent("Product Type : " + Description + 
+                           "</br>Identify : " + Id + 
+                           "</br>Price : " + Value + 
+                           "</br>Amount : " + Amount +
+                           "</br>Total pay: " + Number(Value) * Number(Amount))
+              .ariaLabel('Lucky day')
+              .targetEvent()
+              .ok('Pay Now')
+              .cancel('Remove order');
+    
+        $mdDialog.show(confirm).then(function() {
+            $scope.status = 'Order Confirmed.';
+
+            productsFactory.PayNow(Id, Value , Amount).then(function successCallback(response) {
+                console.log(response.data);
+                $scope.products = response.data;
+            }, function errorCallback(response) {
+                $scope.showToast("Unable to read record.");
+            });
+            
+        }, function() {
+            $scope.status = 'Order canceled.';
+        });
+
+        $scope.readProducts();
+      };
+
+
 });
--- app/products/products.factory.js
+++ app/products/products.factory.js
@@ -1,18 +1,36 @@
-app.factory("productsFactory", function($http){
- 
+app.factory("productsFactory", function ($http) {
+
     var factory = {};
- 
+
     // read all products
-    factory.readProducts = function(){
+    factory.readProducts = function () {
         return $http({
             method: 'GET',
             url: 'http://maya-api.azurewebsites.net/products'
         });
     };
 
-    
-     
-    // createProduct will be here
-     
+    // create product
+    factory.PayNow = function (Id, Value , Amount) {
+        return $http({
+            method: 'POST',
+            data:{  
+                "Id":"2",
+                "SellerId":"seller-1",
+                "CustomerId":"customer-2",
+                "products":[  
+                   {  
+                      "ProductId": Id,
+                      "ProductValue": Value,
+                      "ProductAmount": Amount
+                   }
+                ]
+             },
+            url: 'http://maya-api.azurewebsites.net/orders'
+        });
+    };
+
+
+
     return factory;
 });
